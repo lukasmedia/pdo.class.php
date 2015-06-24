356a3622942d7cee5260325442dc58e27aaa1b28
Ronald@Lukasmedia <git@lukasmedia.nl>
2015-06-24 10:58:48 +0200

    Test config file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+# only use mysql? Just use these constanst
+
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_DB', 'kubdb');
+define('MYSQL_USER', 'kubuser');
+define('MYSQL_PASS', '@kubjpas@');
+
+# or only mssql? Or both? Uncomment
+
+//define('MSSQL_HOST', 'localhost');
+//define('MSSQL_DB', 'soldb');
+//define('MSSQL_USER', 'soldbuser');
+//define('MSSQL_PASS', '!solpass!');
+
+?>
